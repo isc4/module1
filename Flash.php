6544fe9228f46f3415178e9076aab88c9753eb08
isc4 <ignat.chegodaykin@gmail.com>
2020-03-02 17:51:52 +0500

    Update Flash.php

--- Flash.php
+++ Flash.php
@@ -4,21 +4,48 @@
 
 /*
 * Class Message
-*
+* @method success()
+* @method error()
 * @method make()
 */
 
-
 class Message {
 
-    // 1. Флэш сообщения
+    public $class;
+
+    // 1. Тип сообщения при положительном результате (SUCCESS)
     /*
-    * make( string $message) : string
+    * success( string $message)
+    */
+ 
+    public function success($message)
+    {
+        $_SESSION['message'] = $message;
+        $this->class = 'container-fluid alert alert-success';
+        $this->make();
+        
+    }
+
+        // 1. Тип сообщения при отрицательном результате (ERROR)
+    /*
+    * error( string $message)
     */
 
-    public static function make($message)
+    public function error($message)
+    {
+        $_SESSION['message'] = $message;
+        $this->class = 'container-fluid alert alert-danger';
+        $this->make();
+    }
+
+    // Вывод Flash сообщения
+
+    public function make()
     {   
-       return $_SESSION['message'] = $message;
+        if (isset($_SESSION['message'])) {
+            echo '<div class="' . $this->class . '">' . $_SESSION['message'] . '</div>';
+            unset($_SESSION['message']);
+        }
   
     }
 }
